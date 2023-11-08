784ed3336f99ec2252b778794bb0c487d2ad4091
Ícaro Emanuel <34634165+IcaroEmanuel@users.noreply.github.com>
2023-11-08 02:23:23 -0300

    Rotas Login e Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: "sessions#new"
   get 'rooms', to: 'rooms#index'
+
+  get 'sessions/new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
+  resources :sessions
+  resources :users, only: [:new, :create]
 end
