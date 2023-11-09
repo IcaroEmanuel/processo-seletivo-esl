c8f3dd20c0a8ddf89dde0f5baf2d1bb6788f93f4
Ícaro Emanuel <34634165+IcaroEmanuel@users.noreply.github.com>
2023-11-08 23:02:47 -0300

    Rota details: Renderiza os respectivos ids Json

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,4 +4,18 @@ class RoomsController < ApplicationController
     @weekdays = Weekday.all
     @timeslots = Timeslot.all
   end
+
+  def details
+    timeslot_id = params[:timeslot_id]
+    weekday_id = params[:weekday_id]
+
+    timeslot = Timeslot.find_by(id: timeslot_id)
+    weekday = Weekday.find_by(id: weekday_id)
+
+    render json: {
+      start_time: timeslot.start_time.strftime('%H:%M'),
+      weekday: weekday.name,
+      user: current_user.name,
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "sessions#new"
   get 'rooms', to: 'rooms#index'
+  get 'rooms/details', to: 'rooms#details'
 
-  get 'sessions/new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
